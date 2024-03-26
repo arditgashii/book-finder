125a3d4993d680fb35894fa7b87aa21260ae975c
gashiardit <arditfcb58@gmail.com>
2024-03-26 05:11:39 +0100

    Added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent
+    BooksComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
