0be929c35593ac6b5a6aeb15a80a892f61136631
gashiardit <arditfcb58@gmail.com>
2024-03-28 00:58:56 +0100

    Added function to show detailed view of book information(with book ID)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { SearchBookComponent } from './books/book-search/book-search.component';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
 
 const routes: Routes = [
   {path: 'about-us', component: AboutUsComponent},
-  { path: '', component: SearchBookComponent }
+  { path: '', component: SearchBookComponent },
+  { path: 'book-details/:id', component: BookDetailsComponent }
 ];
 
 @NgModule({
--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BooksService } from '../services/books.service';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.css']
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit {
+  bookId: string = '';
+  bookDetails: any;
 
+  constructor(private route: ActivatedRoute, private booksService: BooksService) { }
+
+  ngOnInit(): void {
+    // Get book ID from route parameters
+    this.route.params.subscribe(params => {
+      this.bookId = params['id'];
+      // Fetch the details of the selected book
+      this.fetchBookDetails();
+    });
+  }
+
+  fetchBookDetails() {
+    this.booksService.getBookDetails(this.bookId)
+      .subscribe((data: any) => {
+        if (data) {
+          this.bookDetails = data.volumeInfo;
+        }
+      });
+  }
 }
