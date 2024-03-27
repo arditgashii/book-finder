790ce6100b9f183b813fabc098cb81c313afb696
gashiardit <arditfcb58@gmail.com>
2024-03-27 11:50:54 +0100

    Added input and search button to show results while searching a book

--- src/app/books/search-book/search-book.component.ts
+++ src/app/books/search-book/search-book.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BooksServiceService } from '../books.service';
 
 @Component({
   selector: 'app-search-book',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-book.component.css']
 })
 export class SearchBookComponent {
+  searchText: string = '';
+  books: any[] = [];
 
+  constructor(private booksService: BooksServiceService) {}
+
+  search() {
+    this.booksService.searchBooks(this.searchText)
+      .subscribe((data: any) => {
+        this.books = data.items;
+        console.log(data);
+      });
+  }
 }
